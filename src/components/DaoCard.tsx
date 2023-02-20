0bdc770a0586d309bdbe738fd27aa1b3bd786ebb
GHesericsu <60202455+GHesericsu@users.noreply.github.com>
2023-02-20 16:44:38 +0000

    feat: style homepage

--- src/components/DaoCard.tsx
+++ src/components/DaoCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import mountain from '@/svg/mountain.svg';
 import placeholderImage from '@/svg/placeholderImage.svg';
 
 interface DaoCardProps {
@@ -14,21 +15,30 @@ interface DaoCardProps {
 const DaoCard = (props: DaoCardProps) => {
   return (
     <div
-      className={`card z-0 m-1 h-60 w-56 break-words text-center shadow-xl hover:cursor-pointer hover:bg-neutral-focus`}>
+      className={`dao-card card z-0 m-1 h-60 w-56 break-words text-center shadow-xl hover:cursor-pointer`}>
       <Link href={`/dao/${encodeURIComponent(props.daoId)}`}>
         <div className='card-body text-center'>
-          <p className='mb-2 flex items-center justify-center'>
+          <div className='dao-image mb-2 flex items-center justify-center hover:hidden'>
             <Image
               src={placeholderImage}
               alt='placeholder'
               height={60}
               width={60}
             />
-          </p>
-          <h4 className='z-10 mb-1 text-base-content mix-blend-normal'>
-            {props.daoName}
-          </h4>
-          <p className='text-sm text-accent'>{`DAO ID: ${props.daoId}`}</p>
+            <div className='absolute'>
+              <Image
+                src={mountain}
+                alt='mountain'
+                width={30}
+                height={17}></Image>
+            </div>
+          </div>
+          <div className='overflow-visible'>
+            <h4 className='z-10 text-base-content mix-blend-normal'>
+              {props.daoName}
+            </h4>
+            <p className='text-sm text-accent'>{`DAO ID: ${props.daoId}`}</p>
+          </div>
           <p className='mt-5 text-sm underline underline-offset-2'>{`See more >`}</p>
         </div>
       </Link>
