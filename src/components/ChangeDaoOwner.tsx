6881dc958bd4be10c61c428c179454a27e5c5fba
joh@deep-ink.ventures <joh@deep-ink.ventures>
2023-08-22 23:14:01 +0800

    added change dao owner action and getAssetHolding service for TransferAsset action

--- src/components/ChangeDaoOwner.tsx
+++ src/components/ChangeDaoOwner.tsx
@@ -1,15 +1,12 @@
-import useGenesisDao from '@/hooks/useGenesisDao';
+// import useGenesisDao from '@/hooks/useGenesisDao';
 import useGenesisStore from '@/stores/genesisStore';
 
 const ChangeDaoOwner = (props: { daoId: string; assetId: number | null }) => {
   const currentWalletAccount = useGenesisStore((s) => s.currentWalletAccount);
-  const { makeBatchTransferTxn } = useGenesisDao();
+  // const { makeBatchTransferTxn } = useGenesisDao();
   const txnProcessing = useGenesisStore((s) => s.txnProcessing);
-  const updateTxnProcessing = useGenesisStore((s) => s.updateTxnProcessing);
+  // const updateTxnProcessing = useGenesisStore((s) => s.updateTxnProcessing);
   const handleTransferDao = () => {
-    if (!currentWalletAccount) {
-      return;
-    }
     // todo: invoke makeBatchTransferTxn
   };
 
