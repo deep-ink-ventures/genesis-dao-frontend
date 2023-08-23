11543cecb84e0be7dcb26f77037a58a0b16f1a8d
GHesericsu <60202455+GHesericsu@users.noreply.github.com>
2023-08-24 00:23:57 +0100

    docs: add comment

--- src/services/multiSigTransactions.ts
+++ src/services/multiSigTransactions.ts
@@ -75,6 +75,8 @@ interface ListMultiSigTxnsQueryParams {
   offset?: number;
 }
 
+
+// fixme waiting to for service to add back filter by dao_id
 // const get = async (address: string) => {
 //   const response = await fetch(`${SERVICE_URL}/multisig-transactions/${address}`);
 
