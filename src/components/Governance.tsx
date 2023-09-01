c81e42d4ba2bb4f1f2fc818641d7c27688d98e00
GHesericsu <60202455+GHesericsu@users.noreply.github.com>
2023-09-01 22:39:31 +0100

    fix: disable destroy dao and fix ui bugs

--- src/components/Governance.tsx
+++ src/components/Governance.tsx
@@ -1,8 +1,8 @@
 import useGenesisStore from '@/stores/genesisStore';
 
 import ChangeDaoOwner from './ChangeDaoOwner';
-import DestroyDao from './DestroyDao';
-import TransferAsset from './TransferTreasuryAsset';
+import DestroyMultiSigDao from './DestroyMultiSigDao';
+import TransferTreasuryAsset from './TransferTreasuryAsset';
 
 const Governance = () => {
   const [currentWalletAccount, currentDao] = useGenesisStore((s) => [
@@ -25,7 +25,7 @@ const Governance = () => {
         )}
         {currentWalletAccount && currentDao && (
           <>
-            <div className='flex items-center gap-2 rounded-[8px] border-[0.3px] border-neutral-focus p-4 hover:cursor-pointer'>
+            <div className='flex items-center gap-2 rounded-[8px] border-[0.3px] border-neutral-focus p-4'>
               <div className='flex w-1/6 shrink-0'>Change Owner</div>
               <div className='flex grow'>
                 Transfer Ownership of the DAO to another account
@@ -34,26 +34,23 @@ const Governance = () => {
                 <ChangeDaoOwner />
               </div>
             </div>
-            <div className='flex items-center gap-2 rounded-[8px] border-[0.3px] border-neutral-focus p-4 hover:cursor-pointer'>
+            <div className='flex items-center gap-2 rounded-[8px] border-[0.3px] border-neutral-focus p-4'>
               <div className='flex w-1/6 shrink-0'>Transfer Asset</div>
               <div className='flex grow'>
                 Transfer tokens from your treasury to other accounts
               </div>
               <div className='flex'>
-                <TransferAsset />
+                <TransferTreasuryAsset />
               </div>
             </div>
-            <div className='flex items-center gap-2 rounded-[8px] border-[0.3px] border-neutral-focus p-4 hover:cursor-pointer'>
+            <div className='flex items-center gap-2 rounded-[8px] border-[0.3px] border-neutral-focus p-4'>
               <div className='flex w-1/6 shrink-0'>Destroy DAO</div>
               <div className='flex grow'>
                 Say goodbye and wrap up. Your stake will be repaid to the
                 current DAO Owner
               </div>
               <div className='flex'>
-                <DestroyDao
-                  daoId={currentDao.daoId}
-                  assetId={currentDao.daoAssetId}
-                />
+                <DestroyMultiSigDao />
               </div>
             </div>
           </>
