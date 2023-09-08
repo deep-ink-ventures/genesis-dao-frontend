b7c3b9ce8b5bbaecf3920fc4d868e3c3cd01fef3
joh@deep-ink.ventures <joh@deep-ink.ventures>
2023-09-08 19:37:20 +0800

    mock components not needed in test

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 
 import Index from '../src/pages/index';
 // eslint-disable-next-line
 jest.mock('next/router', () => require('next-router-mock'));
+
+jest.mock('../src/components/CreateDaoModal');
+jest.mock('../src/components/ExploreDaos');
 
 describe('Index page', () => {
   describe('Render method', () => {
